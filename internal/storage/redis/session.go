58c12ae6bd31118c5217688d25bd8fed50937e04
Stepan <s@pstnv.ru>
2026-03-01 00:17:08 +0300

    [*]: rewrite redis storage to hash table

--- internal/storage/redis/session.go
+++ internal/storage/redis/session.go
@@ -5,11 +5,16 @@ import (
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 
-	"github.com/ogen-go/ogen/json"
 	"github.com/redis/go-redis/v9"
 
 	"ndbx/internal/storage/redis/dto"
+)
+
+const (
+	createdAtField = "created_at"
+	updatedAtField = "updated_at"
 )
 
 type SessionStorage struct {
@@ -32,8 +37,8 @@ func (s *SessionStorage) SetOrUpdate(ctx context.Context, req *dto.SetOrUpdateRe
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	var rawOldValue []byte
-	if err := s.client.Get(ctx, sessionKey(req.SID)).Scan(&rawOldValue); err != nil {
+	createdAtRaw, err := s.client.HGet(ctx, sessionKey(req.SID), createdAtField).Result()
+	if err != nil {
 		if errors.Is(err, redis.Nil) {
 			if err := s.setValue(ctx, &dto.SetReq{
 				SID:   req.NewSID,
@@ -46,17 +51,17 @@ func (s *SessionStorage) SetOrUpdate(ctx context.Context, req *dto.SetOrUpdateRe
 			return &dto.SetOrUpdateResp{SID: req.NewSID, IsCreated: true}, nil
 		}
 
-		return nil, fmt.Errorf("get: %w", err)
+		return nil, fmt.Errorf("hget: %w", err)
 	}
 
-	var oldValue dto.SessionValue
-	if err := json.Unmarshal(rawOldValue, &oldValue); err != nil {
-		return nil, fmt.Errorf("unmarshal value: %w", err)
+	createdAt, err := time.Parse(time.RFC3339, createdAtRaw)
+	if err != nil {
+		return nil, fmt.Errorf("parse %q field: %w", createdAtField, err)
 	}
 
 	if err := s.setValue(ctx, &dto.SetReq{
 		SID:   req.SID,
-		Value: dto.SessionValue{CreatedAt: oldValue.CreatedAt, UpdatedAt: req.NewValue.UpdatedAt},
+		Value: dto.SessionValue{CreatedAt: createdAt, UpdatedAt: req.NewValue.UpdatedAt},
 		TTL:   req.TTL,
 	}); err != nil {
 		return nil, err
@@ -66,22 +71,35 @@ func (s *SessionStorage) SetOrUpdate(ctx context.Context, req *dto.SetOrUpdateRe
 }
 
 func (s *SessionStorage) Get(ctx context.Context, req *dto.GetReq) (*dto.GetResp, error) {
-	var value dto.SessionValue
-	if err := s.client.Get(ctx, sessionKey(req.SID)).Scan(&value); err != nil {
-		return nil, fmt.Errorf("get: %w", err)
+	res, err := s.client.HMGet(ctx, sessionKey(req.SID), createdAtField, updatedAtField).Result()
+	if err != nil {
+		return nil, fmt.Errorf("hmget: %w", err)
+	}
+	if len(res) != 2 || res[0] == nil || res[1] == nil {
+		return nil, fmt.Errorf("invalid : %w", redis.Nil)
 	}
 
-	return &dto.GetResp{SessionValue: value}, nil
+	createdAt, err := parseTime(res[0], time.RFC3339, createdAtField)
+	if err != nil {
+		return nil, err
+	}
+	updatedAt, err := parseTime(res[1], time.RFC3339, updatedAtField)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.GetResp{SessionValue: dto.SessionValue{CreatedAt: createdAt, UpdatedAt: updatedAt}}, nil
 }
 
 func (s *SessionStorage) setValue(ctx context.Context, req *dto.SetReq) error {
-	rawData, err := json.Marshal(req.Value)
-	if err != nil {
-		return fmt.Errorf("marshal value: %w", err)
-	}
-
-	if err := s.client.Set(ctx, sessionKey(req.SID), rawData, req.TTL).Err(); err != nil {
-		return fmt.Errorf("set: %w", err)
+	pipe := s.client.TxPipeline()
+	pipe.HSet(ctx, sessionKey(req.SID),
+		createdAtField, req.Value.CreatedAt.UTC().Format(time.RFC3339),
+		updatedAtField, req.Value.UpdatedAt.UTC().Format(time.RFC3339),
+	)
+	pipe.Expire(ctx, sessionKey(req.SID), req.TTL)
+	if _, err := pipe.Exec(ctx); err != nil {
+		return fmt.Errorf("hset with expire: %w", err)
 	}
 
 	return nil
@@ -89,4 +107,18 @@ func (s *SessionStorage) setValue(ctx context.Context, req *dto.SetReq) error {
 
 func sessionKey(sid string) string {
 	return "sid:" + sid
+}
+
+func parseTime(t any, layout string, field string) (time.Time, error) {
+	timeStr, ok := t.(string)
+	if !ok {
+		return time.Time{}, fmt.Errorf("invalid %q field type: expected: string", field)
+	}
+
+	parsedTime, err := time.Parse(layout, timeStr)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("parse %q field: %w", field, err)
+	}
+
+	return parsedTime, nil
 }
